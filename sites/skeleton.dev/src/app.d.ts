1f010351bbda9aa16e71045b7bf33857ac13478d
shirey25 <coreyshirey@premiumdesignpartners.com>
2023-06-11 15:31:19 -0400

    ts

--- sites/skeleton.dev/src/app.d.ts
+++ sites/skeleton.dev/src/app.d.ts
@@ -1,7 +1,22 @@
 /// <reference types="@sveltejs/kit" />
 /// <reference types="vite-plugin-sveld" />
 
-// See https://kit.svelte.dev/docs/types#the-app-namespace
+import { SupabaseClient, Session } from '@supabase/supabase-js'
+import { Database } from './DatabaseDefinitions'
+
+declare global {
+  namespace App {
+    interface Locals {
+      supabase: SupabaseClient<Database>
+      getSession(): Promise<Session | null>
+    }
+    interface PageData {
+      session: Session | null
+    }
+    // interface Error {}
+    // interface Platform {}
+  }
+}/ See https://kit.svelte.dev/docs/types#the-app-namespace
 // for information about these interfaces
 declare namespace App {
 	// interface Locals {}
