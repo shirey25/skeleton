060d0cd69ff68f680e4f6eed09058f89d84dcc05
shirey25 <coreyshirey@premiumdesignpartners.com>
2023-06-11 22:05:51 -0400

    te
    tsts

--- sites/skeleton.dev/src/routes/(inner)/docs/quickstart/+page.svelte
+++ sites/skeleton.dev/src/routes/(inner)/docs/quickstart/+page.svelte
@@ -78,9 +78,9 @@ npm run dev
 	<h1 class="h1">Hello Skeleton</h1>
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 	<section>
-		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
-		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
-		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
+		<a class="btn variant-ghost-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+		<a class="btn variant-ghost-secondary" href="https://tailwindcss.com/">Tailwind</a>
+		<a class="btn variant-ghost-tertiary" href="https://github.com/">GitHub</a>
 	</section>
 </div>
 `}
