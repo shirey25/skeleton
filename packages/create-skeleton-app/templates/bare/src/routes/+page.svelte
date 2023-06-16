ad24b6b4bac8d9bd7367a6a9e4977880182d55e0
shirey25 <coreyshirey@premiumdesignpartners.com>
2023-06-16 13:26:47 -0400

    111

--- packages/create-skeleton-app/templates/bare/src/routes/+page.svelte
+++ packages/create-skeleton-app/templates/bare/src/routes/+page.svelte
@@ -3,17 +3,17 @@
 	
 </script>
 
-<svelte:head>
+<svelte:head> </svelte:head>	
 	<title>Home</title>
 	<AppShell>
 
 	<slot></slot>
-</AppShell>
+
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
-</head>
-<body>
 	
-</body>
-</html>
+	</AppShell>
+
+
+</style>
